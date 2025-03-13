7b8893a3141ab51ecf713d5b3e55178ab9a234a3
“Heisenberg” <ruku508501@gmail.com>
2025-03-13 00:11:30 -0400

    SauceLabsLogin.cy.ts: update test to log in and verify product label 🛒🔑

--- cypress/e2e/SauceLabsLogin.cy.ts
+++ cypress/e2e/SauceLabsLogin.cy.ts
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('.product_label').should('have.text', 'Products')
   })
 })
