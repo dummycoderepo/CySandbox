eef62477e8273d091a72dbd50f1e61560f732c37
“Heisenberg” <ruku508501@gmail.com>
2025-03-13 00:13:08 -0400

    .vscode/settings.json => update commit message format instructions ✏️ cypress/e2e/SauceLabsLogin.cy.ts => add a failing test case for login functionality ❌

--- cypress/e2e/SauceLabsLogin.cy.ts
+++ cypress/e2e/SauceLabsLogin.cy.ts
@@ -6,4 +6,13 @@ describe('template spec', () => {
     cy.get('#login-button').click()
     cy.get('.product_label').should('have.text', 'Products')
   })
+
+  //Add a test that fails
+  it('fails', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('.product_label').should('have.text', 'Product')
+  })
 })
