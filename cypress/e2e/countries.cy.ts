e7c5ac167b383196d225d50a76e247d4908a4372
“Heisenberg” <ruku508501@gmail.com>
2025-04-07 03:48:23 -0400

    ⚙️ cypress/e2e/countries.cy.ts => Refactored country data table test to focus on 'Bahamas' and removed unused currency functions

--- cypress/e2e/countries.cy.ts
+++ cypress/e2e/countries.cy.ts
@@ -2,43 +2,10 @@ it("Country Data Table", () => {
   cy.visit("http://127.0.0.1:5500/HTML/countryData.html");
   cy.get("table").should("exist");
 
-  cy.contains("tr", "El Salvador")
-    .invoke("index")
-    .then((index: number) => {
-      cy.log((index + 1).toString());
+  cy.contains('tr', 'Bahamas').invoke('index').print()
+  cy.contains('tr', 'Bahamas').then((row) => {
+    let rowVal = row.text();
+    cy.log(JSON.stringify(rowVal.split("\t")))
+  })
 
-      getCurrency(index + 1).then((currency: string[]) => {
-        cy.log(currency[0]);
-      });
-    });
-
-  let uniqueCurr = new Set<string>();
-  getCurrencyColVals()
-    .then((currency: string[]) => {
-      currency.forEach((curr, index) => {
-        if (!uniqueCurr.has(curr)) {
-          uniqueCurr.add(curr);
-        } else {
-          cy.log(`Duplicate currency: ${curr} + at index ${index}`);
-        }
-      });
-    })
-    .then(() => {
-      uniqueCurr.forEach((curr) => {
-        getCurrencyColVals().then((currency: string[]) => {
-          const occurrences = currency.filter((c) => c === curr).length;
-          if (occurrences > 1) {
-            cy.log(`Currency ${curr} is duplicated ${occurrences} times`);
-          }
-        });
-      });
-    });
-});
-
-function getCurrency(index: number) {
-  return cy.get(`:nth-child(${index}) > :nth-child(4)`).map("innerText");
-}
-
-function getCurrencyColVals() {
-  return cy.get(`:nth-child(4)`).map("innerText");
-}
+})
