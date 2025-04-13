74a228a3b6c46441d64c81b4f3d29e5fc8bbcaaf
“Heisenberg” <ruku508501@gmail.com>
2025-04-13 15:29:43 -0400

    ✨ Bingo.cy.ts => Added a new test for the Bingo game functionality

---
+++ cypress/e2e/Bingo.cy.ts
@@ -0,0 +1,23 @@
+it("Bigno", () => {
+  cy.visit("http://127.0.0.1:5500/HTML/bingoGame.html");
+
+  let bingoFound = false;
+
+  cy.get("button").each((btn, index) => {
+    //cy.then(() => {
+      if (bingoFound) {
+        return; // Stop iterating if bingo is found
+      } else {
+        cy.wrap(btn).click();
+        cy.contains("Bingo!")
+          .should(Cypress._.noop)
+          .then((bing): void => {
+            if (bing.length > 0) {
+              bingoFound = true;
+              cy.log("Bingo found at index: " + index);
+            }
+          });
+      }
+    });
+  //});
+});
